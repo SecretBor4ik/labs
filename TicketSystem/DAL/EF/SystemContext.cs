2ee4cfb08d650d02c880e1911091113ada0ee1da
SecretBor4ik <131820859+SecretBor4ik@users.noreply.github.com>
2024-12-26 22:42:59 +0200

    Added EF and Repos impl

---
+++ TicketSystem/DAL/EF/SystemContext.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+    public class SystemContext : DbContext
+    {
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<User> Users { get; set; }
+        public SystemContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+    }
+}
